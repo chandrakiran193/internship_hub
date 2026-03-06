e882865855c290e0490e87cc35a6053d31dc74f2
Neelam chandra kiran <24eg107h25@anurag.edu.in>
2026-03-06 23:05:40 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+function toggleAuth(type){
+// your JS logic
+}
+function performLogin(){
+// login logic
+}
